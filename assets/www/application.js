b0061c1a9f830549649aee051ae8e09d4a459de7
abhilash <abhidsm@gmail.com>
2011-05-21 20:46:57 +0530

    Implemented animation to show the result

--- assets/www/application.js
+++ assets/www/application.js
@@ -1,10 +1,6 @@
 x$(window).load(function(e){
-	bindFormValidation();
 	bindFormSubmitAction();
     });
-
-function bindFormValidation() {
-}
 
 function bindFormSubmitAction() {
     x$("#submit").on('click', function(e){
@@ -15,27 +11,35 @@ function bindFormSubmitAction() {
 function formSubmitAction() {
     yourName = x$('#your_name')[0].value;
     partnerName = x$('#partner_name')[0].value;
-    x$("#result").xhr('http://flamesgame.appspot.com/show', {
-	    async:true,
-	    method:'post',
-		data: 'security=&your_name='+yourName+'&partner_name='+partnerName,
-		callback: function() {
-		/*var result = eval("("+this.responseText+")").flames.result;
-		var status = eval("("+this.responseText+")").flames.status;
-		x$('#result h3').html(result);
-		resetResultClass();
-		x$('#result').addClass(status);*/
-		alert('asd');
-	    }	
-	});
+    if(yourName != "" && partnerName != "") { 
+	x$('#submit').fade('out');
+	var params = 'security=&your_name='+yourName+'&partner_name='+partnerName;
+	x$("#result").xhr('http://flamesgame.appspot.com/show', {
+		async:true,
+		    method:'post',
+		    data: params,
+		    callback: function() {
+		    returnData = this.responseText;
+		    var result = eval(returnData).flames.result;
+		    var status = eval(returnData).flames.status;
+		    x$('#result h3').html(result);
+		    resetResultClass();
+		    x$('#result').addClass(status);
+		}	
+	    });
+    }
 }
 
 function resetResultClass(){
     x$('#result').removeClass();
     x$('#result').addClass('center');
-
-    //  setTimeout(function(){ x$('#load').hide();  x$('#submit').show();     $('#result').fadeIn('slow').delay(2000).fadeOut('slow'); }, 2000);
-
+    x$('#result').setStyle('display', 'block');
+    x$('#result').tween({opacity:'1', duration:2000});
+    x$('#result').tween({opacity:'0', duration:2000}, function(){
+	    x$('#result').setStyle('display', 'none');
+	    x$('#result').setStyle('opacity', '1');
+	    x$('#submit').fade('in');
+	});
 }
 
  
